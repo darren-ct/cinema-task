dac5456376188ae20570af39b1e2cec31c10488f
Darren Christian <93717921+darren-ct@users.noreply.github.com>
2022-08-22 11:18:59 +0700

    Update movies.js

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const verifyAdmin = require("../middleware/verifyAdmin");
 const {uploadFile} = require("../middleware/uploadFile");
 
-const {  getMovies, getMovie ,getMyMovies  , postMovie, editMovie, deleteMovie } = require("../controller/movies");
+const {  getMyMovies  , postMovie, editMovie, deleteMovie } = require("../controller/movies");
 
 
-router.get("/movies", getMovies);
-router.get("/movie/:id", getMovie);
 router.get("/mymovies",getMyMovies);
 router.post("/movie", verifyAdmin, uploadFile("image"), postMovie); 
 router.put("/movie/:id", verifyAdmin, uploadFile("image"), editMovie);
@@ -15,5 +13,3 @@ router.delete("/movie/:id",verifyAdmin, deleteMovie);
 
 
 module.exports = router;
-
-
